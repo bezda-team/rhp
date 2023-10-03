1eb373a490f7899dcf53fbe94d7bac61d5db82f5
Anas Bouzid <anasbouzid@gmail.com>
2023-10-02 21:50:51 -0500

    update formatting

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -39,9 +39,7 @@ const Bar = ({ item }:{item: Observable<{id: string | undefined, barIndex: numbe
     const trackedIndex = index.use()
 
     const sanitizedMarkup = useSelector(() => {
-
         // console.log(trackedIndex)
-
         let newMarkup = item.markup.get();
         if (item.markup.get() !== undefined){
             Object.keys(vars).forEach((key) => {
